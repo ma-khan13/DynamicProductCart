998defd76c3e30bc9d1fd7f32976a942d0325b55
MKASIF <mdasifkhan089@gmail.com>
2022-02-14 01:01:01 +0600

    Project Done

--- app.js
+++ app.js
@@ -1,68 +1,38 @@
-
-
-
-// function clickedBtm(productQuantity,productPrice, price) {
-//     // document.getElementById('phone-btn-plus').addEventListener('click', function () {
-//     let phoneQuantity = document.getElementById(productQuantity)
-//     let phoneQuantityNumber = phoneQuantity.value;
-//     phoneQuantity.value = parseFloat(phoneQuantityNumber) + 1;
-//     phoneTotalPrice = document.getElementById(productPrice).innerText = phoneQuantity.value * price
-//     getTotal();
-// // })
-// }
-
-document.getElementById('phone-btn-plus').addEventListener('click', function () {
-    let phoneQuantity = document.getElementById('phone-quantity')
+function clickedBtn(productQuantity,productPrice, price , quantityPlus , type) {
+    let phoneQuantity = document.getElementById(productQuantity)
     let phoneQuantityNumber = phoneQuantity.value;
-    phoneQuantity.value = parseFloat(phoneQuantityNumber) + 1;
-    phoneTotalPrice = document.getElementById('phone-price').innerText = phoneQuantity.value * 1219
-    getTotal();
-    // clickedBtm('phone-quantity' , 'phone-price', 1219)
-})
-
-// clickedBtm()
-document.getElementById('phone-btn-minus').addEventListener('click', function () {
-    let phoneQuantity = document.getElementById('phone-quantity')
-    let phoneQuantityNumber = phoneQuantity.value;
-    if (phoneQuantityNumber > 0) {
-        phoneQuantity.value = parseFloat(phoneQuantityNumber) - 1
-        let phonePrice = document.getElementById('phone-price');
-        let  currentPrice = phonePrice.innerText;
-        phonePrice.innerText = parseFloat(currentPrice) - 1219;
-        getTotal();
+    if (quantityPlus) {
+        phoneQuantity.value = parseFloat(phoneQuantityNumber) + 1;
+        totalPhonePrice = document.getElementById(productPrice).innerText = phoneQuantity.value * price;
+    } else if(phoneQuantityNumber > 0){
+        phoneQuantity.value = parseFloat(phoneQuantityNumber) - 1;
+        let phonePrice = document.getElementById(productPrice);
+        let pevPrice = phonePrice;
+        pevPrice.innerText = parseFloat(pevPrice.innerText) - price;
     }
+    getTotal();
+}
+document.getElementById('phone-btn-plus').addEventListener('click', function () {
+    clickedBtn('phone-quantity' , 'phone-price', 1219 , true)
 })
-
-
-
-
+document.getElementById('phone-btn-minus').addEventListener('click', function () {
+    clickedBtn('phone-quantity' , 'phone-price', 1219 , false )
+})
 document.getElementById('case-btn-plus').addEventListener('click', function () {
-    let caseQuantity = document.getElementById('case-quantity')
-    let caseQuantityNumber = caseQuantity.value;
-    caseQuantity.value = parseFloat(caseQuantityNumber) + 1;
-    document.getElementById('case-price').innerText = caseQuantity.value * 59;
-    getTotal()
-    // clickedBtm('case-quantity' , 'case-price', 59)
-
+    clickedBtn('case-quantity' , 'case-price', 59 , true)
 })
 document.getElementById('case-btn-minus').addEventListener('click', function () {
-    let caseQuantity = document.getElementById('case-quantity')
-    let caseQuantityNumber = caseQuantity.value;
-    if (caseQuantityNumber > 0) {
-        caseQuantity.value = parseFloat(caseQuantityNumber) - 1
-        let casePrice = document.getElementById('case-price');
-        let  currentPrice = casePrice.innerText;
-        casePrice.innerText = parseFloat(currentPrice) - 59;
-        getTotal()
-    }
+    clickedBtn('case-quantity' , 'case-price', 59 , false)
 })
 
 function getTotal() {
     const phonePrice = document.getElementById('phone-price').innerText;
     const casePrice = document.getElementById('case-price').innerText;
-    const subTotal = document.getElementById('sub-total').innerText = parseFloat(phonePrice) + parseFloat(casePrice);
-    const tax = document.getElementById('tax').innerText = (parseFloat(phonePrice) / 10 + parseFloat(casePrice) / 10).toFixed(2)
-    document.getElementById('total').innerText = (parseFloat(subTotal) + parseFloat(tax)).toFixed(2);
+    const subTotal = document.getElementById('sub-total');
+    const tax = document.getElementById('tax');
+    const total = document.getElementById('total'); 
+    const subTotalPrice = subTotal.innerText = parseInt(phonePrice) + parseInt(casePrice);
+    const totalTax = tax.innerText = subTotalPrice / 10;
+    total.innerText = subTotalPrice + totalTax;
 }
-
 getTotal()
